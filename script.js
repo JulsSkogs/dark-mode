1444c46d682205ec88dc3851d4c1a6fa2877fb8d
JulsSkogs <rjsollenskog@gmail.com>
2023-01-17 16:14:50 -0500

    refactor

--- script.js
+++ script.js
@@ -16,38 +16,38 @@ function imageMode(color) {
   image3.src = `img/undraw_conceptual_idea_${color}.svg`;
 }
 
-// Further refactor darkMode() and lightMode()
-// function toggleMode(isDark) {
-//   nav.style.backgroundColor = isDark
-//     ? "rgb(0 0 0 / 50%)"
-//     : "rgb(255 255 255 / 50%)";
-//   textBox.style.backgroundColor = isDark
-//     ? "rgb(255 255 255 / 50%)"
-//     : "rgb(0 0 0 / 50%)";
-//   toggleIcon.children[0].textContent = isDark ? "Dark Mode" : "Light Mode";
-//   isDark
-//     ? toggleIcon.children[1].classList.replace("fa-sun", "fa-moon")
-//     : toggleIcon.children[1].classList.replace("fa-moon", "fa-sun");
-//   isDark ? imageMode("dark") : imageMode("light");
-// }
+Further refactor darkMode() and lightMode()
+function toggleMode(isDark) {
+  nav.style.backgroundColor = isDark
+    ? "rgb(0 0 0 / 50%)"
+    : "rgb(255 255 255 / 50%)";
+  textBox.style.backgroundColor = isDark
+    ? "rgb(255 255 255 / 50%)"
+    : "rgb(0 0 0 / 50%)";
+  toggleIcon.children[0].textContent = isDark ? "Dark Mode" : "Light Mode";
+  isDark
+    ? toggleIcon.children[1].classList.replace("fa-sun", "fa-moon")
+    : toggleIcon.children[1].classList.replace("fa-moon", "fa-sun");
+  isDark ? imageMode("dark") : imageMode("light");
+}
 
 // // Dark Mode Styles
-function darkMode() {
-  nav.style.backgroundColor = "rgb(0 0 0 / 50%)";
-  textBox.style.backgroundColor = "rgb(255 255 255 / 50%)";
-  toggleIcon.children[0].textContent = "Dark Mode";
-  toggleIcon.children[1].classList.replace("fa-sun", "fa-moon");
-  imageMode("dark");
-}
+// function darkMode() {
+//   nav.style.backgroundColor = "rgb(0 0 0 / 50%)";
+//   textBox.style.backgroundColor = "rgb(255 255 255 / 50%)";
+//   toggleIcon.children[0].textContent = "Dark Mode";
+//   toggleIcon.children[1].classList.replace("fa-sun", "fa-moon");
+//   imageMode("dark");
+// }
 
-// Light Mode Styles
-function lightMode() {
-  nav.style.backgroundColor = "rgb(255 255 255 / 50%)";
-  textBox.style.backgroundColor = "rgb(0 0 0 / 50%)";
-  toggleIcon.children[0].textContent = "Light Mode";
-  toggleIcon.children[1].classList.replace("fa-moon", "fa-sun");
-  imageMode("light");
-}
+// // Light Mode Styles
+// function lightMode() {
+//   nav.style.backgroundColor = "rgb(255 255 255 / 50%)";
+//   textBox.style.backgroundColor = "rgb(0 0 0 / 50%)";
+//   toggleIcon.children[0].textContent = "Light Mode";
+//   toggleIcon.children[1].classList.replace("fa-moon", "fa-sun");
+//   imageMode("light");
+// }
 
 // Switch Theme Dynamically
 function switchTheme(event) {
